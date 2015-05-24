e8a756164935b79fa682c015640ac50f5b6b6237
puf <puf@google.com>
2015-05-24 16:56:38 -0700

    Fix a few bugs, added screenshot

--- app/src/main/java/com/firebase/firebaseofflinequeries/MainActivity.java
+++ app/src/main/java/com/firebase/firebaseofflinequeries/MainActivity.java
@@ -37,8 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -141,11 +139,6 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
